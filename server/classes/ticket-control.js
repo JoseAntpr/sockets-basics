f44b2ee3a695f9c471f402e802e2b6800f55d25d
JoseAntpr <joseantpalaciosramirez@gmail.com>
2018-08-29 12:18:45 +0200

    ADD: get pending tickets

--- server/classes/ticket-control.js
+++ server/classes/ticket-control.js
@@ -1,17 +1,26 @@
 const fs = require('fs');
 
+class Ticket {
+    constructor(ticketNumber, desk) {
+        this.ticketNumber = ticketNumber;
+        this.desk = desk;
+    }
+}
+
 
 class TicketControl {
 
     constructor() {
         this.last = 0;
         this.today = new Date().getDate();
+        this.tickets = [];
 
         let data = require('../data/data.json');
 
         if ( data.today === this.today ) {
 
             this.last = data.last;
+            this.tickets = data.tickets;
 
         } else {
             this.resetCont();
@@ -20,6 +29,10 @@ class TicketControl {
 
     nextTicket() {
         this.last += 1;
+        
+        let ticket = new Ticket(this.last, null);
+        this.tickets.push(ticket);
+        
         this.saveFile();
 
         return `Ticket ${ this.last }`
@@ -39,7 +52,8 @@ class TicketControl {
 
         let jsonData = {
             last: this.last,
-            today: this.today
+            today: this.today,
+            tickets: this.tickets
         };
 
         let jsonDataString = JSON.stringify(jsonData);
